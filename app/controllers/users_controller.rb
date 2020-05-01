e2b4160415997738e725cbdfd04a7c2a48a36c6f
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-01 19:38:28 +0900

    引数の変更
    Useridを渡すようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.valid?
-            token = encode_token({user_id: @user.id})
+            token = encode_token(@user.id)
             render json: {user: @user, token: token}
         else
             render json: {error: "Invalid username or password"}
