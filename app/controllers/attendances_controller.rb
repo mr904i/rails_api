5143c93342560d13d55f88e5f245a9a8cb9e26e3
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-10 13:35:12 +0900

    認証アクションの作成
    attendance_params関数で
    Applicationコントローラーでユーザー認証用のコールバック関数を呼び出し実行しuser_idを取得し、postしたハッシュにuser_i
    d keyを追加し、DBにpost格納するレコードを作成した
    Createアクション内でDBにpost実行、saveできたらjsonを返す処理を実行する

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,28 @@
 class AttendancesController < ApplicationController
     def index
-        @example_json = {'hello' => 'world'}
-        render json: @example_json
+        #自分のレコードだけ表示する
+        example_json = {'hello' => 'world'}
+        render json: example_json
     end
+    #打刻アクション
+    def create
+        @attendance = Attendance.new(attendance_params)
+
+        # response
+        if @attendance.save
+            render json: @attendance, status: :created, location: attendances_url
+        else
+            render json: @attendance.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def attendance_params
+        #ユーザーidもこちらに移動する
+        attendance_params = params.require(:attendance).permit(:date, :start_time, :end_time)
+        user_id = user_authentication
+        attendance_params["user_id"] = user_id
+        return attendance_params
+    end
+
 end
