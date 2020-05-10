001508e4c62aefb95a7219c1709e9088adaba112
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-10 11:32:18 +0900

    routeの追加
    Attendance controllerが作成できたのでrouteを作成し、エンドポイントよりアクションにアクセスできるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :drivers
   resource :users, only: [:create]
+  #resource :attendances, only: [:index]
+  resources :attendances, only: [:index]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
