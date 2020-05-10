b1466ad0e7cec60119d511d405dc4ef4410455fb
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-10 13:35:26 +0900

    Createアクションをrouteに追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :drivers
   resource :users, only: [:create]
   #resource :attendances, only: [:index]
-  resources :attendances, only: [:index]
+  resources :attendances, only: [:index, :create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
